2cb7e6e5665e9ece63eb08fdc03d834d70c3c781
lam0620 <lam0620@gmail.com>
2024-11-17 11:21:34 +0700

    Add log

--- backend/apps/report/views/views_worklist.py
+++ backend/apps/report/views/views_worklist.py
@@ -175,6 +175,7 @@ class WorklistView(OrderBaseView):
 
         # Convert queryset to json, merge df_study to df_merged
         df_merged = self._merge_df(queryset, df_study, status)
+        logger.debug('Number of rows after merging df_study: %s', len(df_merged))
 
         # Search status = SC or IM in df. Do this because the status in procedure table is not latest data
         if status and (status == 'SC' or status == 'IM'):
@@ -204,8 +205,10 @@ class WorklistView(OrderBaseView):
         
         # Get rows duplicate accession_no
         df_duplciate_order=df_order[df_order.duplicated(subset=['accession_no'],keep=False)]
+        logger.debug('Number of duplicated rows: %s', len(df_duplciate_order))
         # Get new df by removing duplicated rows
         df_new_order = df_order.drop_duplicates(subset=['accession_no'], keep=False) 
+        logger.debug('Number of rows after drop duplicated: %s', len(df_new_order))
 
         # Merge df_study to df_new_order
         df_merged = df_new_order.merge(df_study, how='left', on='accession_no')
